312102dfcc12d7fedebcab0245dfe8828161839f
ezezbogar <ezezbogar@gmail.com>
2020-11-22 20:59:31 -0300

    Documento System

--- server_src/server_system.h
+++ server_src/server_system.h
@@ -16,12 +16,23 @@ class System {
     /* Constructor */
     explicit System(std::string rootFile);
 
+    /* Constructor por copia borrado */
+    System(const System& system) = delete;
+
+    /* Asignacion por copia borrado */
+    System operator=(const System& system) = delete;
+
+    /* Devuelve un string con el contenido del "archivo"
+     * con el nombre de fileName */
     std::string getFile(std::string fileName);
 
+    /* Guarda el contenido de file con el nombre de fileName */
     void loadFile(const std::string& fileName, const std::string& file);
 
+    /* Printea por pantalla el contenido de str (thread-safe) */
     void printOnScreen(const std::string& str);
 
+    /* Devuelve un string con el nombre del rootfile */
     std::string getRootFile();
 
     /* Destructor */
